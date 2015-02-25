5f6279619bc4232d6ba3d93c915c8dd7f096cced
Ogs <ogsmelvin05@gmail.com>
2015-02-26 04:08:43 +0800

    site angular js

---
+++ assets/js/app/controllers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var ctrls = angular.module('elstudio.controllers.site', [
+  'elstudio.services'
+]);
+
+ctrls.controller('NotFoundCtrl', function ($scope) {
+
+});
+
+ctrls.controller('SiteCtrl', function ($scope){
+
+});
+
+ctrls.controller('AccountCtrl', function ($scope) {
+
+});
