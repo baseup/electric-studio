b76d335103c82178e1512a42805b7e8ada53c2ad
Ogs <ogsmelvin05@gmail.com>
2015-03-12 23:25:05 +0800

    add pagination to history

--- assets/js/app/controllers.js
+++ assets/js/app/controllers.js
@@ -555,7 +555,7 @@ ctrls.controller('ClassCtrl', function ($scope, $location, SharedService, BookSe
   }
 });
 
-ctrls.controller('HistoryCtrl', function ($scope, HistoryService) {
+ctrls.controller('HistoryCtrl', function ($scope, $routeParams, HistoryService) {
   if (!$scope.loginUser || $scope.loginUser.length == 0) {
     $location.path("/")
     angular.element("html, body").animate({ scrollTop: 0 }, "slow");
@@ -564,10 +564,57 @@ ctrls.controller('HistoryCtrl', function ($scope, HistoryService) {
 
     $('#history-tabs').Tab();
 
+    $scope.currentScheds = 0;
+    $scope.currentTrans = 0;
+
     $scope.histories = HistoryService.query();
     $scope.histories.$promise.then(function (data) {
       $scope.histories = data;
     });
+
+    $scope.prevScheds = function(event){
+      if($scope.currentScheds > 0){
+        $scope.currentScheds -= 1;
+        $scope.histories = HistoryService.query({ schedPage : $scope.currentScheds });
+        $scope.histories.$promise.then(function (data) {
+          $scope.histories = data;
+        });
+      }
+      event.preventDefault();
+    }
+
+    $scope.nextScheds = function(event){
+      if($scope.currentScheds < parseInt($scope.histories.schedsTotal)){
+        $scope.currentScheds += 1;
+        $scope.histories = HistoryService.query({ schedPage : $scope.currentScheds });
+        $scope.histories.$promise.then(function (data) {
+          $scope.histories = data;
+        });
+      }
+      event.preventDefault();
+    }
+
+    $scope.prevTrans = function(event){
+      if($scope.currentTrans > 0){
+        $scope.currentTrans -= 1;
+        $scope.histories = HistoryService.query({ transPage : $scope.currentTrans });
+        $scope.histories.$promise.then(function (data) {
+          $scope.histories = data;
+        });
+      }
+      event.preventDefault();
+    }
+
+    $scope.nextTrans = function(event){
+      if($scope.currentTrans > parseInt($scope.histories.transTotal)){
+        $scope.currentTrans += 1;
+        $scope.histories = HistoryService.query({ transPage : $scope.currentTrans });
+        $scope.histories.$promise.then(function (data) {
+          $scope.histories = data;
+        });
+      }        
+      event.preventDefault();
+    }
 
   }
 
