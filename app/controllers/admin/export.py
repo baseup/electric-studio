dfadf70325af2af6ec7ca93cdad6080ed16f856e
Anthony De Leon <tonton.dev001@gmail.com>
2015-03-18 22:11:39 +0800

    Apply direct to prod changes

--- app/controllers/admin/export.py
+++ app/controllers/admin/export.py
@@ -4,21 +4,21 @@ from datetime import datetime
 import csv
 
 def download_bookings(self):
-    date = self.get_query_argument('date')
-    time = self.get_query_argument('time')
-    if not date:
-        date = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
-    else:
-        date = datetime.strptime(date, '%Y-%m-%d')
-    time = datetime.strptime(time, '%I:%M %p')
-    ins_sched = yield InstructorSchedule.objects.get(date=date, start_time=time)
-    if not ins_sched:
-        ins_sched = yield InstructorSchedule.objects.get(day=date.strftime('%a').lower(), start=time)
-    bookings = yield BookedSchedule.objects.filter(date=date, schedule=ins_sched).find_all()
+    #date = self.get_query_argument('date')
+    #time = self.get_query_argument('time')
+    #if not date:
+    #    date = datetime.strptime(datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
+    #else:
+    #    date = datetime.strptime(date, '%Y-%m-%d')
+    #time = datetime.strptime(time, '%I:%M %p')
+    #ins_sched = yield InstructorSchedule.objects.get(date=date, start_time=time)
+    #if not ins_sched:
+    #    ins_sched = yield InstructorSchedule.objects.get(day=date.strftime('%a').lower(), start=time)
+    bookings = yield BookedSchedule.objects.find_all()
     filename = 'bookings-' + datetime.now().strftime('%Y-%m-%d %H:%I') + '.csv'
 
     with open(filename, 'w') as csvfile:
-        fieldnames = ['first_name', 'last_name', 'seat number', 'status', 'date', 'time']
+        fieldnames = ['first_name', 'last_name', 'seat number', 'status', 'date']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for b in bookings:
@@ -27,8 +27,7 @@ def download_bookings(self):
                 'last_name': b.user_id.last_name,
                 'seat number': b.seat_number,
                 'status': b.status,
-                'date': b.date.strftime('%Y-%m-%d'),
-                'time': b.schedule.start.strftime('%I:%M %p')
+                'date': b.date.strftime('%Y-%m-%d')
             })
 
     buf_size = 4096
