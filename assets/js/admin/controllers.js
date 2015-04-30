2935b402aa1a737f583e354788cbfb6c0b7d46c5
Melvin Maron <ogsmelvin05@gmail.com>
2015-04-30 14:19:36 +0800

    Put bike move button in account schedules

--- assets/js/admin/controllers.js
+++ assets/js/admin/controllers.js
@@ -398,6 +398,38 @@ ctrls.controller('AccountCtrl', function ($scope, $timeout, $location, UserServi
     }
   });  
 
+  $scope.moveBikeModal = function (book) {
+    ClassService.query({ date: book.date.replace(' 00:00:00',''), sched_id: book.schedule._id, seats: true }, function (seats) {
+      $scope.selectedBook = book;
+      if (seats.available.length) {
+        var selectize = angular.element('#switch-seat')[0].selectize;
+        selectize.settings.sortField = 'text';
+        angular.forEach(seats.available, function (seat) {
+          selectize.addOption({ value: seat, text: seat });
+        });
+
+        angular.element('#switch-bike-modal').Modal();
+      } else{
+        $.Notify({ content: 'No seats available to switch' });    
+      }
+    });
+  }
+
+  $scope.moveBike = function () {
+    if ($scope.selectedBike) {
+      var confirm_msg = 'Are you sure to switch you bike (' + $scope.selectedBook.seat_number + ') to ' + $scope.selectedBike + '?';
+      $.Confirm(confirm_msg, function () {
+        ClassService.update({ scheduleId: $scope.selectedBook._id }, { move_to_seat : $scope.selectedBike }, function () {
+          $scope.filterSchedDate($scope.selectedAccount);
+        }, function (error) {
+          $.Notify({ content: error.data });
+        });
+      });
+    } else {
+      $.Alert('Please select bike to switch')
+    }
+  }
+
   $scope.cancelWaitlist = function (index, sched) {
     ClassService.delete({ scheduleId: sched._id }, function () {
       $scope.filterSchedDate($scope.selectedAccount);
