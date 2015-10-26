601d8aacef7f58788b5f6e53aad329c631d7ddd8
Melvin Maron <ogsmelvin05@gmail.com>
2015-10-26 23:25:37 +0800

    remove dst code on GMT8 convertion

--- app/helper.py
+++ app/helper.py
@@ -24,19 +24,11 @@ class Lock(object):
         if key in Lock.locker:
             del Lock.locker[key]
 
-
 class GMT8(tzinfo):
     def utcoffset(self, dt):
-        return timedelta(hours=7) + self.dst(dt)
+        return timedelta(hours=8)
     def dst(self, dt):
-        d = datetime(dt.year, 4, 1)
-        self.dston = d - timedelta(days=d.weekday() + 1)
-        d = datetime(dt.year, 11, 1)
-        self.dstoff = d - timedelta(days=d.weekday() + 1)
-        if self.dston <=  dt.replace(tzinfo=None) < self.dstoff:
-            return timedelta(hours=1)
-        else:
-            return timedelta(0)
+        return timedelta(0)
     def tzname(self,dt):
         return "GMT +8"
 
